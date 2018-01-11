6a76f96a89bbcd690d62a7788272f674a09d17f7
zmingzhi <zmingzhi@yeah.net>
2018-01-11 17:50:29 +0800

    2.0

--- src/ToolProvider.php
+++ src/ToolProvider.php
@@ -1,18 +1,21 @@
-<?php namespace zongphp\tool;
+<?php
+namespace zongphp\tool;
 
 use zongphp\framework\build\Provider;
 
-class ToolProvider extends Provider {
+class ToolProvider extends Provider
+{
+    //延迟加载
+    public $defer = true;
 
-	//延迟加载
-	public $defer = true;
+    public function boot()
+    {
+    }
 
-	public function boot() {
-	}
-
-	public function register() {
-		$this->app->single( 'Tool', function ( $app ) {
-			return new Tool();
-		} );
-	}
+    public function register()
+    {
+        $this->app->single('Tool', function ($app) {
+            return new Tool();
+        });
+    }
 }
